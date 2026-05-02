a73868b2e42206d8513e32a88515a96a2e9e7e8a
TLayol <th.trung1110@gmail.com>
2026-05-02 16:12:03 +0700

    Add Register.jsx component for Task 7

---
+++ server/frontend/src/components/Register/Register.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import "./Register.css";
+
+const Register = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const register = async (e) => {
+    e.preventDefault();
+    let register_url = window.location.origin + "/djangoapp/register";
+    
+    const res = await fetch(register_url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        "userName": userName,
+        "password": password,
+        "firstName": firstName,
+        "lastName": lastName,
+        "email": email
+      }),
+    });
+
+    const json = await res.json();
+    if (json.status === "Authenticated") {
+      window.location.href = window.location.origin;
+    }
+  };
+
+  return (
+    <div className="register_container">
+      <form onSubmit={register}>
+        <h2>Sign Up</h2>
+        <input type="text" placeholder="Username" onChange={(e) => setUserName(e.target.value)} required/>
+        <input type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} required/>
+        <input type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} required/>
+        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required/>
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required/>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
